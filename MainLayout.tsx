3e62cffddaabb1a8a0a32fa3ebfffd9573d083fb
Rajat_Agarwal <rajat18agarwal5689@gmail.com>
2026-05-13 12:41:22 +0530

    whitespace fixes

--- MainLayout.tsx
+++ MainLayout.tsx
@@ -1,27 +1,71 @@
+// import { Box, Toolbar } from "@mui/material";
+// import { Outlet } from "react-router-dom";
+
+// import Navbar from "../navbar/Navbar";
+// import Sidebar from "../sidebar/Sidebar";
+
+// const MainLayout = () => {
+//   return (
+//     <Box sx={{ display: "flex" }}>
+//       {/* TOP NAVBAR */}
+//       <Navbar />
+
+//       {/* SIDEBAR */}
+//       <Sidebar />
+
+//       {/* PAGE CONTENT */}
+//       <Box
+//         component="main"
+//         sx={{
+//           flexGrow: 1,
+//           p: 3,
+//         }}
+//       >
+//         {/* SPACE BELOW NAVBAR */}
+//         <Toolbar />
+
+//         {/* ROUTE PAGE */}
+//         <Outlet />
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default MainLayout;
+
+
+
 import { Box, Toolbar } from "@mui/material";
+
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../navbar/Navbar";
-import Sidebar from "../sidebar/Sidebar";
 
 const MainLayout = () => {
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box
+      sx={{
+        minHeight: "100vh",
+
+        width: "100%",
+
+        background:
+          "linear-gradient(to bottom right, #EEF2FF, #F8FAFC)",
+      }}
+    >
       {/* TOP NAVBAR */}
       <Navbar />
-
-      {/* SIDEBAR */}
-      <Sidebar />
 
       {/* PAGE CONTENT */}
       <Box
         component="main"
         sx={{
-          flexGrow: 1,
-          p: 3,
+          width: "100%",
+
+          boxSizing: "border-box",
         }}
       >
-        {/* SPACE BELOW NAVBAR */}
+        {/* SPACE BELOW FIXED NAVBAR */}
         <Toolbar />
 
         {/* ROUTE PAGE */}
